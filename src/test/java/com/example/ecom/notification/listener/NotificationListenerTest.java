24490f4cb1ce3b22dd094ff9a9a6b81811f05baa
Matthew Campbell <matt@moderne.io>
2025-12-31 17:15:51 -0500

    Baseline
    Co-authored-by: Moderne <team@moderne.io>

--- src/test/java/com/example/ecom/notification/listener/NotificationListenerTest.java
+++ src/test/java/com/example/ecom/notification/listener/NotificationListenerTest.java
@@ -1,7 +1,8 @@
 package com.example.ecom.notification.listener;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class NotificationListenerTest {
 
